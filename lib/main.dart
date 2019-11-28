4ea2d968ebbe142708b39e717b1c42eaf400dbb3
HeversonAtaide <senaheverson@gmail.com>
2019-11-28 18:19:53 -0300

    init
    Criação do projeto

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_ij_ideia_01/menu_dashboard_layout.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: MenuDashboardPage(),
+    );
+  }
+}
