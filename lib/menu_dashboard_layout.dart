13df12e006135144b2a152b4608a3d58132fce25
HeversonAtaide <senaheverson@gmail.com>
2019-12-02 16:44:56 -0300

    commit

--- lib/menu_dashboard_layout.dart
+++ lib/menu_dashboard_layout.dart
@@ -1,5 +1,7 @@
-import 'package:dots_indicator/dots_indicator.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sparkline/flutter_sparkline.dart';
 import 'package:page_indicator/page_indicator.dart';
 
 final Color backgroundColor = Color(0xFF4A4A58);
@@ -19,7 +21,17 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
   Animation<double> _menuScaleAnimation;
   Animation<Offset> _slideAnimation;
   List<String> _meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
-  int currentPageValue;
+
+  PageController pageController3 = PageController();
+  var currentPageValue = 0.0;
+
+  var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
+  double _currentPage = 0;
+  PageController _pageController = PageController(
+    initialPage: 0,
+    viewportFraction: 0.8,
+
+  );
 
   @override
   void initState() {
@@ -30,8 +42,14 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
         Tween<double>(begin: 0.5, end: 1).animate(_controller);
     _slideAnimation = Tween<Offset>(begin: Offset(-1, 0), end: Offset(0, 0))
         .animate(_controller);
+
+    pageController3.addListener(() {
+      setState(() {
+        currentPageValue = pageController3.page;
+      });
+    });
   }
-  https://dribbble.com/shots/6351511-Menu-and-Dashboard
+
   @override
   void dispose() {
     _controller.dispose();
@@ -103,6 +121,20 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
 
   Widget dashboard(BuildContext context) {
 
+    /*Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (_currentPage < 3) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: Duration(milliseconds: 150),
+        curve: Curves.easeIn,
+      );
+    });*/
+
     return AnimatedPositioned(
       duration: duration,
       top: 0,
@@ -152,51 +184,9 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
                   SizedBox(height: 20),
                   Container(
                     height: 300,
-                    child: PageIndicatorContainer(
-                      child: PageView(
-                        onPageChanged: (position){
-                          getChangedPageAndMoveBar(position);
-                          setState(() {
-
-                          });
-
-                        },
-                        controller: PageController(viewportFraction: 0.8),
-                        scrollDirection: Axis.horizontal,
-                        pageSnapping: true,
-                        children: <Widget>[
-                          Material(
-                            borderRadius: BorderRadius.all(Radius.circular(40)),
-
-                          ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8),
-                            color: Colors.redAccent,
-                            width: 100,
-                          ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8),
-                            color: Colors.blueAccent,
-                            width: 100,
-                          ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8),
-                            color: Colors.greenAccent,
-                            width: 100,
-                          ),
-                        ],
-                      ),
-                      align: IndicatorAlign.bottom,
-                      length: 4,
-                      indicatorSpace: 10.0,
-                      padding: EdgeInsets.all(10),
-                      shape: IndicatorShape.circle(size: 8),
-                      indicatorColor: Colors.black12,
-                      indicatorSelectorColor: Colors.blueAccent,
-
-                    ),
+                    child: pages(),
                   ),
-                  Stack(
+                  /*Stack(
                     alignment: AlignmentDirectional.topStart,
                     children: <Widget>[
                       Container(
@@ -214,7 +204,7 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
                         ),
                       ),
                     ],
-                  ),
+                  ),*/
                   SizedBox(height: 20),
                   Text("Transactions", style: TextStyle(color: Colors.white, fontSize: 20),),
                   ListView.separated(
@@ -245,6 +235,124 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
     );
   }
 
+  Widget pages(){
+    return PageIndicatorContainer(
+      child: PageView(
+        onPageChanged: (position){
+          getChangedPageAndMoveBar(position);
+          setState(() {
+
+          });
+
+        },
+        controller: _pageController,
+        scrollDirection: Axis.horizontal,
+        pageSnapping: true,
+        children: <Widget>[
+          Material(
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            child: new Sparkline(
+              data: data,
+            ),
+
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            color: Colors.redAccent,
+            width: 100,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            color: Colors.blueAccent,
+            width: 100,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            color: Colors.greenAccent,
+            width: 100,
+          ),
+        ],
+      ),
+      align: IndicatorAlign.bottom,
+      length: 4,
+      indicatorSpace: 10.0,
+      padding: EdgeInsets.all(10),
+      shape: IndicatorShape.circle(size: 8),
+      indicatorColor: Colors.black12,
+      indicatorSelectorColor: Colors.blueAccent,
+
+    );
+  }
+
+  Widget pages2(){
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        Container(
+          width: 160.0,
+          color: Colors.red,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.green,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.yellow,
+        ),
+        Container(
+          width: 160.0,
+          color: Colors.orange,
+        ),
+      ],
+    );
+  }
+
+  Widget pages3(){
+    return PageView.builder(
+      controller: pageController3,
+      itemBuilder: (context, position) {
+        if (position == currentPageValue.floor()) {
+          return Transform.scale (
+            scale: currentPageValue,
+            child: Container (
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              height: 200.0,
+              width: 200.0,
+              color: Colors.yellow ,
+            ),
+          );
+        } else if (position == currentPageValue.floor() + 1){
+          return Transform.scale (
+            scale: currentPageValue % 1,
+            child: Container (
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              height: 200.0,
+              width: 200.0,
+              color: Colors.yellow ,
+            ),
+          );
+        } else {
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            color: position % 2 == 0 ? Colors.blue : Colors.pink,
+            child: Center(
+              child: Text(
+                "Page",
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+            ),
+          );
+        }
+      },
+      itemCount: 10,
+    );
+  }
+
   Widget circleBar(bool isActive) {
     return AnimatedContainer(
       duration: Duration(milliseconds: 150),
@@ -257,7 +365,7 @@ class _MenuDashboardPageState extends State<MenuDashboardPage>
     );
   }
   void getChangedPageAndMoveBar(int page) {
-    currentPageValue = page;
+    //currentPageValue = page;
     setState(() {});
   }
 }
